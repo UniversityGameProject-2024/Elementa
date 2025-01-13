b3860c50aaed67eb4dabe916dd439e52360e7542
Nikita <anosov.nikita@gmail.com>
2025-01-14 00:25:41 +0200

    enemy actions

--- Assets/Scripts/Enemies/Knight/Projectile.cs
+++ Assets/Scripts/Enemies/Knight/Projectile.cs
@@ -1,55 +1,55 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class Projectile : MonoBehaviour
-{
-    [SerializeField] private float speed;
-    private float direction;
-    private bool hit;
-    private float lifetime;
+//public class Projectile : MonoBehaviour
+//{
+//    [SerializeField] private float speed;
+//    private float direction;
+//    private bool hit;
+//    private float lifetime;
 
-    private Animator anim;
-    private BoxCollider2D boxCollider;
+//    private Animator anim;
+//    private BoxCollider2D boxCollider;
 
-    private void Awake()
-    {
-        anim = GetComponent<Animator>();
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
-    private void Update()
-    {
-        if (hit) return;
-        float movementSpeed = speed * Time.deltaTime * direction;
-        transform.Translate(movementSpeed, 0, 0);
+//    private void Awake()
+//    {
+//        anim = GetComponent<Animator>();
+//        boxCollider = GetComponent<BoxCollider2D>();
+//    }
+//    private void Update()
+//    {
+//        if (hit) return;
+//        float movementSpeed = speed * Time.deltaTime * direction;
+//        transform.Translate(movementSpeed, 0, 0);
 
-        lifetime += Time.deltaTime;
-        if (lifetime > 5) gameObject.SetActive(false);
-    }
+//        lifetime += Time.deltaTime;
+//        if (lifetime > 5) gameObject.SetActive(false);
+//    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        hit = true;
-        boxCollider.enabled = false;
-        anim.SetTrigger("explode");
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        hit = true;
+//        boxCollider.enabled = false;
+//        anim.SetTrigger("explode");
 
-        if (collision.tag == "Enemy")
-            collision.GetComponent<Health>().TakeDamage(1);
-    }
-    public void SetDirection(float _direction)
-    {
-        lifetime = 0;
-        direction = _direction;
-        gameObject.SetActive(true);
-        hit = false;
-        boxCollider.enabled = true;
+//        if (collision.tag == "Enemy")
+//            collision.GetComponent<Health>().TakeDamage(1);
+//    }
+//    public void SetDirection(float _direction)
+//    {
+//        lifetime = 0;
+//        direction = _direction;
+//        gameObject.SetActive(true);
+//        hit = false;
+//        boxCollider.enabled = true;
 
-        float localScaleX = transform.localScale.x;
-        if (Mathf.Sign(localScaleX) != _direction)
-            localScaleX = -localScaleX;
+//        float localScaleX = transform.localScale.x;
+//        if (Mathf.Sign(localScaleX) != _direction)
+//            localScaleX = -localScaleX;
 
-        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
-    }
-    private void Deactivate()
-    {
-        gameObject.SetActive(false);
-    }
-}
+//        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+//    }
+//    private void Deactivate()
+//    {
+//        gameObject.SetActive(false);
+//    }
+//}
