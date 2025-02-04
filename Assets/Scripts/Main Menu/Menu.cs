9ee39a7a4707e03ae82d586567ca2e6e64e07c86
Nikita <anosov.nikita@gmail.com>
2025-02-04 23:22:42 +0200

    add difficulty

--- Assets/Scripts/Main Menu/Menu.cs
+++ Assets/Scripts/Main Menu/Menu.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
-    public void PlayGame()
+    public void Normal()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(2);
     }
     public void ReturnToMenu()
     {
         SceneManager.LoadSceneAsync(0);
+    }
+    public void Easy()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void Hard()
+    {
+        SceneManager.LoadSceneAsync(3);
     }
 }
